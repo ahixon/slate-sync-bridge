d8b319b3b03aeeff6b399cc7478c06e251419e3e
Alex Hixon <alex@alexhixon.com>
2019-06-02 18:34:00 +1000

    fix support for skipping tests

--- test/slate/helpers/fixtures.js
+++ test/slate/helpers/fixtures.js
@@ -16,6 +16,12 @@ export const fixtures = (...args) => {
   const dir = basename(path)
   const d = options.skip ? describe.skip : describe
 
+  // Ensure that the key generator is reset. We have to do this here
+  // because the `require` call will create the Slate objects.
+  beforeEach(() => {
+    KeyUtils.resetGenerator()
+  });
+
   d(dir, () => {
     for (const file of files) {
       const p = resolve(path, file)
@@ -35,18 +41,10 @@ export const fixtures = (...args) => {
       ) {
         const name = basename(file, extname(file))
 
-        // This needs to be a non-arrow function to use `this.skip()`.
-        it(name, function() {
-          // Ensure that the key generator is reset. We have to do this here
-          // because the `require` call will create the Slate objects.
-          KeyUtils.resetGenerator()
-          const module = require(p)
-
-          if (module.skip) {
-            this.skip()
-            return
-          }
-
+        const module = require(p)
+        const i = module.skip ? it.skip : it;
+        
+        i(name, function() {
           fn({ name, path, module })
         })
       }
