9b7dbecd79cca576efc39e425d41a7aef889eb03
Alex Hixon <alex@alexhixon.com>
2019-06-02 13:25:29 +1000

    copy helpers from slate tests

---
+++ test/slate/helpers/h.js
@@ -0,0 +1,48 @@
+import { createHyperscript } from 'slate-hyperscript'
+
+const h = createHyperscript({
+  blocks: {
+    line: 'line',
+    paragraph: 'paragraph',
+    quote: 'quote',
+    code: 'code',
+    list: 'list',
+    item: 'item',
+    image: 'image',
+  },
+  inlines: {
+    link: 'link',
+    hashtag: 'hashtag',
+    comment: 'comment',
+    emoji: 'emoji',
+  },
+  marks: {
+    b: 'bold',
+    i: 'italic',
+    u: 'underline',
+    fontSize: 'font-size',
+  },
+  annotations: {
+    result: 'result',
+    highlight: 'highlight',
+  },
+  schema: {
+    blocks: {
+      image: {
+        isVoid: true,
+      },
+    },
+    inlines: {
+      emoji: {
+        isVoid: true,
+      },
+    },
+    annotations: {
+      result: {
+        isAtomic: true,
+      },
+    },
+  },
+})
+
+export default h
